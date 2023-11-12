94a8b5a7ae5239772b62e05592184b4689265479
odomaj <odomaj@clarkson.edu>
2023-11-11 19:50:08 -0500

    Started Assignment

---
+++ scripts/buildall.py
@@ -0,0 +1,10 @@
+import os
+
+print('bulding project...')
+
+if(os.path.exists(os.getcwd() + '/../build')):
+    os.chdir('../build')
+else:
+    os.chdir('../')
+    os.system('mkdir build')
+    os.chdir('build')
